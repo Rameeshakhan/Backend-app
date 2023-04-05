a60ebde099ce577c936df3f0c76f613c1607599a
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-05 23:45:17 +0500

    Components Added

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p className='pt-2'>Welcome Home</p>
+        <h1>Rameesha You are doing well keep going</h1>
+    </div>
+  )
+}
+
+export default Home
