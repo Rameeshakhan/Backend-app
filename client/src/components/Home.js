6baf3ec9ba5de781d58493254ee91b792d60131b
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-09 04:29:11 +0500

    Modified

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,50 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div style={{position: "relative", zIndex: 1}}>
-        <p className='pt-2'>Welcome Home</p>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-        <h1>Rameesha You are doing well keep going</h1>
-    </div>
+    <>
+    </>
   )
 }
 
