247b796fe07621dc013e9f8285dfb140dd752ca8
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-10 00:49:10 +0500

    Login Page Designed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import register from "./images/register.jpg"
+import login from "./images/login.jpg"
 import email from "./images/gmail.png"
 import password from "./images/password.png"
 
@@ -10,7 +10,7 @@ const Login = () => {
     <div>
        <div className='container'>
        <div className='right-container'>
-          <img className="image" src={register} alt="register" />
+          <img className="image" src={login} alt="register" />
           <Link className="nav-link active" to="/register"><p>Dont have account ? Register here</p></Link>
         </div>
         <div className='left-container'>
@@ -25,7 +25,7 @@ const Login = () => {
             <img className='icon' src={password} alt="password" />
               <input type="password" name="password" placeholder='Password' />
             </div><br />
-            <p className="forgot-password">Forgot Password</p><br/>
+            <p className="forgot-password">Forgot Password</p>
            <button className='button'>Login</button>
           </form>
         </div>
