0d3638d45b3c4ec9e112bc0265ad8744e30712b8
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-11 04:07:00 +0500

    About Page designed

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="about">
+        <h1>About Us</h1>
+        <hr/>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque sapiente totam ratione quae enim. Ea dolorem labore reiciendis beatae provident commodi voluptatibus possimus obcaecati impedit quam iure, corrupti ab quaerat iusto, error odio nihil reprehenderit magni. Possimus provident ipsum consequatur officiis asperiores fugit cum quis reiciendis cumque? Eos, harum ratione! Iure aliquam quo optio voluptates quam officia quasi culpa provident animi! Nobis dolores qui praesentium facere repellendus sapiente vitae in quas quisquam cum eligendi nesciunt, quaerat alias ducimus consequatur cumque? Quibusdam voluptatibus id, dolores, fugiat a aliquid vero animi voluptates libero quas ut numquam deleniti provident autem? Quibusdam blanditiis dignissimos culpa incidunt debitis neque minima dolorem autem velit, sunt ipsam optio sint quia minus repellat. Aperiam ab eius quas consequatur nesciunt. Distinctio, accusamus non maiores consectetur saepe voluptate harum quia veniam doloribus sed quo quos dicta amet?
+        </p>
+      </div>
+    </div>
   )
 }
 
